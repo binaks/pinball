1e816019829f59dc84ac5a3181163013f7e6f363
Bianca <binaksrc@gmail.com>
2020-03-15 20:38:55 -0300

    Add files via upload

---
+++ Assets/Flip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour
+{
+    public enum Flipper { 
+        LEFT,
+        RIGHT
+    };
+
+    public Flipper side;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/InitialPush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialPush : MonoBehaviour
+{
+    Rigidbody r;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
